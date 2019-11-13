b9f36fc7df8541fd7ce3c5e4e604bcc9b0ddee2d
islamaymansais <islam.elkadi.3261@gmail.com>
2019-11-13 14:52:09 -0500

    Update sys_utils.py

--- src/ModelManager/sys_utils.py
+++ src/ModelManager/sys_utils.py
@@ -1,12 +1,10 @@
-from itertools import product
-
 from gensim.models.fasttext import FT
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
-def save_model(model, path):
+def save_model(model,path):
     model.save(path)
     return True
 
 def load_model(path):
-    model = model.load(path)
+    model=model.load(path)
     return model
-
