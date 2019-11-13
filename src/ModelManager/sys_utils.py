2c395ac67ae21145a553f29759cefdb5334c9335
islamaymansais <islam.elkadi.3261@gmail.com>
2019-11-13 14:58:03 -0500

    Update sys_utils.py

--- src/ModelManager/sys_utils.py
+++ src/ModelManager/sys_utils.py
@@ -1,5 +1,5 @@
 from gensim.models.fasttext import FT
-from gensim.models.doc2vec import Doc2Vec as DV, TaggedDocument
+from gensim.models.doc2vec import Doc2Vec as D2V, TaggedDocument
 
 def save_model(model,path):
     model.save(path)
