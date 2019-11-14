e7eec5bde1c479969cacc717d25eeb09beca2e9a
islamaymansais <islam.elkadi.3261@gmail.com>
2019-11-14 14:05:55 -0500

    Update REST.py

--- src/ModelManager/REST.py
+++ src/ModelManager/REST.py
@@ -36,88 +36,22 @@ def clean_data():
 
 @app.route("/train_FastText",methods=["POST"])
 def train_FastText():
-    
-    update=request.json.get("update")
-    params=request.json.get("params")
+   
     train_path=request.json.get("trainPath")
+    train_params=request.json.get("train_params")
+    build_params=request.json.get("build_params")
     model_store_path=request.json.get("savePath")
-    
-    if not params["min_count"]:
-        params["min_count"]=5
-    
-    if not params["size"]:
-        params["size"]=100
         
-    if not params["window"]:
-        kwargs["window"]=5
+    # instantiate model isntance with specified parameters    
+    model_gensim=FT(**train_params)
     
-    if not params["workers"]:
-        params["workers"]=3
-    
-    if not params["alpha"]:
-        params["alpha"]=0.025
-    
-    if not params["min_alpha"]:
-        params["min_alpha"]=0.0001
-    
-    if not params["sg"]:
-        params["sg"]=0
-    
-    if not params["hs"]:
-        params["hs"]=0
-    
-    if not params["seed"]:
-        params["seed"]=1
-    
-    if not params["sample"]:
-        params["sample"]=0.001
-    
-    if not params["negative"]:
-        params["negative"]=5
-    
-    if not params["ns_exponent"]:
-        params["ns_exponent"]=0.75
-    
-    if not params["cbow_mean"]:
-        params["cbow_mean"]=1
-    
-    if not params["iter"]:
-        params["iter"]=5
+    # build model vocab
+    model_gensim.build_vocab(**build_params)
 
-    if not params["sorted_vocab"]:
-        params["sorted_vocab"]=1
-    
-    if not params["batch_words"]:
-        params["batch_words"]=10000
-    
-    if not params["min_n"]:
-        params["min_n"]=3
-    
-    if not params["max_n"]:
-        params["max_n"]=6
-    
-    if not params["word_ngrams"]:
-        params["word_ngrams"]=1
-    
-    if not params["bucket"]:
-        params["bucket"]=2000000
-    
-    
-    model_gensim=FT(**params)
-    
-    if update:
-        model_gensim.build_vocab(corpus_file=corpus_file,update=True)
-    else:
-        model_gensim.build_vocab(corpus_file=corpus_file)
-        
     # train the model
-    model_gensim.train(
-        corpus_file=corpus_file,
-        epochs=model_gensim.epochs,
-        total_examples=model_gensim.corpus_count,
-        total_words=model_gensim.corpus_total_words
-    )
+    model_gensim.train(corpus_file=corpus_file,epochs=model_gensim.epochs,total_examples=model_gensim.corpus_count,total_words=model_gensim.corpus_total_words)
     
+    # save model
     save_model(model_gensim,model_store_path)
     
     return jsonify({"FastText Training":"Success"})
@@ -126,86 +60,24 @@ def train_FastText():
 @app.route("/train_doc2vec",methods=["POST"])
 def train_doc2vec():
     
-    update=request.json.get("update")
-    params=request.json.get("params")
     train_path=request.json.get("trainPath")
     model_store_path=request.json.get("savePath")
+    train_params=request.json.get("train_params")
+    build_params=request.json.get("build_params")
+            
+    # instantiate model isntance with specified parameters    
+    model_gensim=D2V(**train_params)
     
-    if not params["dm"]:
-        params["dm"]=
-        
-    if not params["vector_size"]:
-        kwargs["vector_size"]=
-    
-    if not params["window"]:
-        params["window"]=
-    
-    if not params["alpha"]:
-        params["alpha"]=
-    
-    if not params["min_alpha"]:
-        params["min_alpha"]=
-    
-    if not params["seed"]:
-        params["seed"]=
-    
-    if not params["min_count"]:
-        params["min_count"]=  
-        
-    if not params["max_vocab_size"]:
-        params["max_vocab_size"]=  
- 
-    if not params["sample"]:
-        params["sample"]=  
+    # build model vocab
+    model_gensim.build_vocab(**build_params)
 
-    if not params["workers"]:
-        params["workers"]=  
-
-    if not params["epochs"]:
-        params["epochs"]=  
-
-    if not params["hs"]:
-        params["hs"]=  
-
-    if not params["negative"]:
-        params["negative"]=  
-
-    if not params["ns_exponent"]:
-        params["ns_exponent"]=  
-
-    if not params["dm_mean"]:
-        params["dm_mean"]=  
-
-    if not params["dm_concat"]:
-        params["dm_concat"]=  
-
-    if not params["dm_tag_count"]:
-        params["dm_tag_count"]=  
-
-    if not params["dbow_words"]:
-        params["dbow_words"]=  
-
-    if not params["trim_rule"]:
-        params["trim_rule"]=  
-
-    if not params["callbacks"]:
-        params["callbacks"]=  
-        
-    model_gensim=D2V(**params)
-    
-    if update:
-        model_gensim.build_vocab(corpus_file=corpus_file,update=True)
-    else:
-        model_gensim.build_vocab(corpus_file=corpus_file)
-        
     # train the model
-    model_gensim.train(
-        corpus_file=corpus_file,
-        epochs=model_gensim.epochs,
-        total_examples=model_gensim.corpus_count,
-        total_words=model_gensim.corpus_total_words
-    )
+    model_gensim.train(corpus_file=corpus_file,epochs=model_gensim.epochs,total_examples=model_gensim.corpus_count,total_words=model_gensim.corpus_total_words)
     
+    # remove temporary training data to recuce memorary consumption
+    model_gensim.delete_temporary_training_data(keep_doctags_vectors=True,keep_inference=True)
+    
+    # save model
     save_model(model_gensim,model_store_path)
     
     return jsonify({"FastText Training":"Success"})
